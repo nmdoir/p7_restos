e6afa25dda8b3a58ec83b877ab3e20781ccad706
nmdoir <nmdoir@gmail.com>
2020-03-10 16:32:54 +0100

    added stylesheet, site title and logo

---
+++ src/components/PageTitle.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class PageTitle extends React.Component {
+    render() {
+        return <h2>Hello test</h2>;
+    }
+}
+
+export default PageTitle;
--- src/components/test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Hello extends React.Component {
-    render() {
-        return <div> Hello from test.js</div>;
-    }
-}
-
-export default Hello;
