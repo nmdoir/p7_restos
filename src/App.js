42e78d1c0d811a5a5bb511078c191fca6742d3d0
nmdoir <nmdoir@gmail.com>
2020-05-22 12:51:53 +0200

    fixed style

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import RestoList from "./components/RestoList";
 
 function App() {
   return (
-      <div className="container">
+      <div className="container-fluid">
           <div className="row">
               <div className="col-7">
                   <MapContainer/>
